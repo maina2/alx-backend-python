0b1df6ddc0e727555a9a8d16a97510b7d9b8a673
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-01 23:23:16 +0300

    commits

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
