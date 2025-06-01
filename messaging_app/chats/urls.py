0abe11d73ade54f2526bd1399cd222dd95d959e7
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-01 23:32:37 +0300

    commits

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-# Explicitly define the router using DefaultRouter
+# Explicitly using rest_framework.routers.DefaultRouter() for API routing
 router = DefaultRouter()
 router.register('conversations', ConversationViewSet, basename='conversations')
 router.register('messages', MessageViewSet, basename='messages')
