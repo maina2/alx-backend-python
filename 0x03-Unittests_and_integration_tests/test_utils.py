b693d5012371ba8432c3600c50c4bce4f35accbd
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-01 20:09:11 +0300

    commits

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,10 @@
 """Unit tests for utils module.
 """
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
-from typing import Mapping, Sequence, Any
+from utils import access_nested_map, get_json
+from typing import Mapping, Sequence, Any, Dict
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,6 +30,22 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), f"'{expected_key}'")
+
+
+class TestGetJson(unittest.TestCase):
+    """Test case for get_json function.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url: str, test_payload: Dict, mock_get: Mock) -> None:
+        """Test get_json returns expected payload and calls requests.get once with test_url."""
+        mock_get.return_value = Mock(json=lambda: test_payload)
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
