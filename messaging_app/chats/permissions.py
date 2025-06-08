585b140f3e9466beb8000fab1d4ef63d164a6a81
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-08 12:41:48 +0300

    commits

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,7 +3,14 @@ from rest_framework.permissions import BasePermission
 from .models import Message, Conversation
 
 class IsParticipant(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
+        if request.method in ["PUT", "PATCH", "DELETE"]:
+            if isinstance(obj, Message):
+                return obj.user == request.user
+            return False
         if isinstance(obj, Message):
             return obj.user == request.user or obj.conversation.participants.filter(id=request.user.id).exists()
         if isinstance(obj, Conversation):
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,13 +1,12 @@
 from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
-from .models import Conversation, Message, User
-from .serializers import ConversationSerializer, MessageSerializer
 from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsParticipant
 
 class ConversationViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsParticipant]
-
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     filter_backends = [filters.OrderingFilter]
@@ -22,13 +21,22 @@ class ConversationViewSet(viewsets.ModelViewSet):
 
 class MessageViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsParticipant]
-
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['sent_at']
 
+    def get_queryset(self):
+        conversation_id = self.kwargs.get('conversation_id')
+        if conversation_id:
+            if not Conversation.objects.filter(id=conversation_id, participants=self.request.user).exists():
+                return Response({"detail": "Not a participant"}, status=status.HTTP_403_FORBIDDEN)
+            return Message.objects.filter(conversation_id=conversation_id)
+        return Message.objects.filter(user=self.request.user)
+
     def create(self, request, *args, **kwargs):
+        conversation_id = request.data.get('conversation_id')
+        if not Conversation.objects.filter(id=conversation_id, participants=request.user).exists():
+            return Response({"detail": "Not a participant"}, status=status.HTTP_403_FORBIDDEN)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
