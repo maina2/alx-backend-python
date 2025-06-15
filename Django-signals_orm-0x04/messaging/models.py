77888aa81a15f18e4917f32368c7504926f04b9e
Simon Chanzu <chanzusimon6@gmail.com>
2025-06-15 18:56:23 +0300

    commits

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -29,6 +29,7 @@ class Notification(models.Model):
 class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='edited_messages')
     edited_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
